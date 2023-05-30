a0fcba051990303d129be6beaa7787b2de7db74d
shalevv123 <shalev.riss@gmail.com>
2023-05-30 21:08:42 +0300

    alon is a bully

--- HW3/part1/part1Sol.py
+++ HW3/part1/part1Sol.py
@@ -5,13 +5,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument("password", type=str, help="password to generate key from")
 
 args = parser.parse_args()
-ascii_odrer = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+ascii_order = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
 keygen_order =".5`>AdIG-*8Ee}@BDRFkzw]ZYgrsm1q\\2|3Jj(KuX !':_v6aWf{y\"C;+L<S4HO[0?$bh%/MtoT9,N)&l=xUpV^7~n#ciQP"
 
 def keyFromPass(password):
     key = ""
     for i in range(len(password)):
-        key += ascii_odrer[keygen_order.index(password[i])]
+        key += ascii_order[keygen_order.index(password[i])]
     return key
 
 
