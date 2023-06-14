b66a984ba7dd74deb5b23fa2685309003e1d8d81
pliscov <64476145+pliscov@users.noreply.github.com>
2023-06-14 18:50:01 +0300

    added a bunch of project

---
+++ HW3/part3/codeshook/codesHook.cpp
@@ -0,0 +1,107 @@
+#include "pch.h"
+#include <Windows.h>
+#include <stdio.h>
+// Add additional includes if needed
+// using namespace std;
+
+// for logging. format: log_file << ... << std::endl;
+// #include <fstream> 
+// using std::ofstream;
+//ofstream log_file("log.txt");
+
+// Typedef for the hooked function signature, such as:
+// typedef INT(WINAPI* FUNC_PTR)(SOCKET, char*, int, int); (WINAPI is for _stdcall)
+// or in the simple case:
+typedef LPVOID FUNC_PTR;
+
+// Ptr to the original function
+FUNC_PTR original_func_address;
+
+// Global variables
+DWORD lpProtect = 0;
+LPVOID IAT;
+FUNC_PTR JumpTo;
+
+// Helper function to remove the hook (for the current call):
+void _stdcall remove_hook() {
+	VirtualProtect((char*)IAT, 0x4, PAGE_EXECUTE_READWRITE, &lpProtect);
+	memcpy(IAT, &original_func_address, 0x4);
+	VirtualProtect((char*)IAT, 0x4, PAGE_EXECUTE_READ, &lpProtect);
+}
+
+// Helper function to restore the hook (for the next calls)
+void _stdcall restore_hook() {
+	VirtualProtect((char*)IAT, 0x4, PAGE_EXECUTE_READWRITE, &lpProtect);
+	memcpy(IAT, &JumpTo, 0x4);
+	VirtualProtect((char*)IAT, 0x4, PAGE_EXECUTE_READ, &lpProtect);
+}
+
+// Hook function. Might use helper functions in C, i.e. void _stdcall helper(int num) {}
+// Might even use only a funcHook c function instead!
+
+// hook: print out both strings in strcmp.
+__declspec(naked) void funcHook() {
+	// Restore overriden bytes
+	remove_hook();
+	char* string1;
+	char* string2;
+	int ret_val;
+
+	__asm {
+		push edi
+		mov edi, DWORD PTR [ebp+8]
+		mov string1, edi
+
+		mov edi, DWORD PTR [ebp+12]
+		mov string2, edi
+
+
+		pop edi
+	}
+	ret_val = strcmp(string1, string2);
+	printf("string1 : %s", string1);
+	printf("string2 : %s", string2);
+	restore_hook();
+	__asm {
+		mov eax, ret_val
+		leave
+		ret
+	}
+}
+
+void setHook() {
+	HMODULE h = GetModuleHandle(L"codes.exe");
+	HMODULE h2 = GetModuleHandle(L"coredll.dll");
+
+	if ((h == NULL) || (h2 == NULL)) { return; }
+
+	original_func_address = (FUNC_PTR)GetProcAddress(h2, "strcmp");
+	if (original_func_address == NULL) {
+		return;
+	}
+
+	int addr_beginning_of_our_exe = 0x400000; // change!
+	int addr_func_to_hook_in_IAT = 0x409294; // change!
+	IAT = h + (addr_func_to_hook_in_IAT - addr_beginning_of_our_exe) / 4; // Calc address of address to override in IAT
+
+	JumpTo = (FUNC_PTR)((char*)&funcHook);
+	restore_hook();
+}
+
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		setHook();
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
